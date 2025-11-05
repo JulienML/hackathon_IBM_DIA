39702688a5e394388e570648210b0ab3a0af8523
Noemiemz <noemie.mazepa@edu.devinci.fr>
2025-11-05 16:07:26 +0100

    def des fonctions

---
+++ inference.py
@@ -0,0 +1,29 @@
+def embed_query():
+    pass
+
+
+def retrieve_documents():
+    pass
+
+
+def build_prompt():
+    prompt = f""
+
+
+def generate_answer():
+    pass
+
+
+def rag_inference():
+    pass
+
+
+
+
+
+if __name__ == "__main__":
+    user_question = input("Pose ta question: ")
+    print("\nUser question:\n", user_question)
+
+
+
